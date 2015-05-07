8682cd5227ca778816b46a379e199114193e596e
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-06 23:34:18 -0700

    |feat| hashes string into u8 - odd f32 division though

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,26 @@
 /// hash_one will take the difference
 pub fn hash_one(datum: &str) -> u8 {
     let nums: Vec<u8> = datum.bytes().collect();
-    /// we'll multiply each byte by the difference b/w it and the next byte
-    /// (as a decimal) and then clear out the decimals
-    let mut hashedDatum: f32 = num[0] as f32;
-    println!("here be bytes {:?}", nums);
+    // we'll multiply each byte by the difference b/w it and the next byte
+    // (as a decimal) and then clear out the decimals
+    let mut hashed_datum: f32 = 0.0;
+    let mut iter = nums.iter().peekable();
+
+    while let Some(num) = iter.next() {
+        if iter.peek() != None {
+            let next: u8 = **iter.peek().unwrap();
+            let mut difference: f32 = *num as f32 - next as f32;
+            println!("starting mini difference {:?}", difference);
+            while difference > 0_f32 {
+                difference = difference / 10_f32;
+            }
+            println!("finished mini difference {:?}", difference);
+            hashed_datum = hashed_datum + (*num as f32 * difference).abs();
+        } else {
+            println!("at the end! here be ye datum hashed {:?} {:?}",
+                hashed_datum, hashed_datum.fract());
+        }
+    }
     nums[0]
 }
 #[test]
