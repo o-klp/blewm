0bb4ed502293f406a411b73069169c04fc88808b
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-06 22:07:32 -0700

    |setup| configures bin & lib - tests string functions

---
+++ src/bin.rs
@@ -0,0 +1,7 @@
+extern crate mylib;
+
+use mylib::hash_one;
+
+pub fn main() {
+    hash_one(&"testa");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+pub fn hash_one(datum: &str) -> bool {
+    let v: Vec<u8> = "bors".bytes().collect();
+    println!("here be bytes {:?}", v);
+    println!("here be datum{:?}", datum);
+    true
+}
 #[test]
 fn it_works() {
+    assert!(hash_one("atest"));
 }
