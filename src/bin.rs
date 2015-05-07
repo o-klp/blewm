21870371115acf5d36f16ab0155351cc7521eb6f
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-06 22:24:32 -0700

    |chore| logs test data for testing/brainstorming

--- src/bin.rs
+++ src/bin.rs
@@ -3,5 +3,36 @@ extern crate mylib;
 use mylib::hash_one;
 
 pub fn main() {
-    hash_one(&"testa");
+    hash_one("Amber");
+    hash_one("Bolton");
+    hash_one("Condon");
+    hash_one("Dustin");
+    hash_one("Forrester");
+    hash_one("Greystark");
+    hash_one("Harclay");
+    hash_one("Ironsmith");
+    hash_one("Karstark");
+    hash_one("Lake");
+    hash_one("Marsh");
+    hash_one("Norrey");
+    hash_one("Overton");
+    hash_one("Peat");
+    hash_one("Pool");
+    hash_one("Quagg");
+    hash_one("Redbeard");
+    hash_one("Reed");
+    hash_one("Ryswell");
+    hash_one("Slate");
+    hash_one("Stane");
+    hash_one("Stark");
+    hash_one("Stout");
+    hash_one("Thenn");
+    hash_one("Umber");
+    hash_one("Waterman");
+    hash_one("Wells");
+    hash_one("Whitehill");
+    hash_one("Woodfoot");
+    hash_one("Woods");
+    hash_one("Woolfield");
+    hash_one("Wull");
 }
