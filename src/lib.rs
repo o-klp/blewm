15b2827a294065881ace5d0fb4e09465046d3141
Keenan Lidral-Porter <keenan@surfair.com>
2015-05-09 22:42:36 -0700

    |test| adds tests for hash_three - fails because of duplicates

--- src/lib.rs
+++ src/lib.rs
@@ -96,10 +96,12 @@ mod tests {
             ];
         let mut northern_hash_one = Vec::new();
         let mut northern_hash_two = Vec::new();
+        let mut northern_hash_three = Vec::new();
 
         for house in north_houses.iter() {
             northern_hash_one.push(hash_one(house));
             northern_hash_two.push(hash_two(house));
+            northern_hash_three.push(hash_three(house));
         }
 
         // 33 elements and a range of 255 - a "well distributed" hash function
@@ -107,6 +109,7 @@ mod tests {
         // To test distribution we'll sort the  hashed values
         northern_hash_one.sort_by(|a, b| a.cmp(b));
         northern_hash_two.sort_by(|a, b| a.cmp(b));
+        northern_hash_three.sort_by(|a, b| a.cmp(b));
 
         // & check there's a *relatively* consistent difference b/w elements
         // The better the hashing fn the smaller the interval
@@ -130,16 +133,28 @@ mod tests {
                 }
             }
         }
+        {
+            let mut iter = northern_hash_three.iter().peekable();
+            while let Some(num) = iter.next() {
+                if iter.peek() != None {
+                    let difference: u8 = *iter.peek().unwrap() - *num;
+                    assert!(difference >= 5 || difference <= 10);
+                }
+            }
+        }
 
         // to test for collisions we'll dedup and expect a difference of <= 2,
         // or 3% (33 elements with 1 intentional duplicate - Wull)
         let northern_one_len: usize = northern_hash_one.len();
         let northern_two_len: usize = northern_hash_two.len();
+        let northern_three_len: usize = northern_hash_three.len();
         northern_hash_one.dedup();
         northern_hash_two.dedup();
+        northern_hash_three.dedup();
 
         assert!(northern_one_len - northern_hash_one.len() <= 2);
         assert!(northern_two_len - northern_hash_two.len() <= 2);
+        assert!(northern_three_len - northern_hash_three.len() <= 2);
 
     }
     #[bench]
@@ -149,6 +164,10 @@ mod tests {
     #[bench]
     fn hash_two_bench(b: &mut Bencher) {
         b.iter(|| hash_two("The Professor"));
+    }
+    #[bench]
+    fn hash_three_bench(b: &mut Bencher) {
+        b.iter(|| hash_three("Cutter"));
     }
 }
 
