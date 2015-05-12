087950ced5bd8d8614f1c2eb555d4c809bdb1143
Keenan Lidral-Porter <keenan@surfair.com>
2015-05-11 23:14:55 -0700

    |feat| returns Result<T, E> from bloom.contains - error handling

--- src/lib.rs
+++ src/lib.rs
@@ -63,54 +63,59 @@ pub fn hash_three(datum: &str) -> u8 {
     hashed_datum as u8
 }
 
-pub struct Bloom<'a> {     
-    pub filter: Vec<u8>,   
+#[derive(Debug)]
+pub enum LookupError {
+    FalsePositive
+}
+
+pub struct Bloom<'a> { 
+    pub filter: Vec<u8>, 
     pub data: Vec<&'a str>,
 }
 
 impl<'a> Bloom<'a> {
-    // insert a string value into the data store                    
+    // insert a string value into the data store
     pub fn insert(&mut self, string: &'a str) -> usize {
-        let index_1: usize = hash_one(string) as usize;             
-        let index_2: usize = hash_two(string) as usize;             
-        let index_3: usize = hash_three(string) as usize;           
+        let index_1: usize = hash_one(string) as usize;
+        let index_2: usize = hash_two(string) as usize;
+        let index_3: usize = hash_three(string) as usize;
 
-        self.filter[index_1] = 1;                                  
-        self.filter[index_2] = 1;                                  
-        self.filter[index_3] = 1;                                  
+        self.filter[index_1] = 1;
+        self.filter[index_2] = 1;
+        self.filter[index_3] = 1;
 
-        self.data.push(string);                                    
-        self.data.len()                                            
-    }                                                               
+        self.data.push(string);
+        self.data.len()
+    }
 
     // query if there is a *chance* data store has string value
     fn query(&self, string: &str) -> bool {
-        let index_1: usize = hash_one(string) as usize;        
-        let index_2: usize = hash_two(string) as usize;        
-        let index_3: usize = hash_three(string) as usize;      
+        let index_1: usize = hash_one(string) as usize;
+        let index_2: usize = hash_two(string) as usize;
+        let index_3: usize = hash_three(string) as usize
 
-        if self.filter[index_1] == 0 {                        
-            return false;                                      
-        }                                                      
-        if self.filter[index_2] == 0 {                        
-            return false;                                      
-        }                                                      
-        if self.filter[index_3] == 0 {                        
-            return false;                                      
-        }                                                      
-        true                                                   
-    }                                                          
+        if self.filter[index_1] == 0 {
+            return false;
+        }
+        if self.filter[index_2] == 0 {
+            return false;
+        }
+        if self.filter[index_3] == 0 {
+            return false;
+        }
+        true
+    }
 
     // check if data store *actually* contains string value    
-    pub fn contains(&self, string: &str) -> bool {     
-        if self.query(string) {                              
-            if !self.data.contains(&string) {                 
-                panic!("False positive!!");                    
-            }                                                  
-            return true;                                       
-        }                                                      
-        false                                                  
-    }                                                          
+    pub fn contains(&self, string: &str) -> Result<bool, LookupError> {
+        if self.query(string) {
+            if !self.data.contains(&string) {
+               return Err(LookupError::FalsePositive);
+            }
+            return Ok(true);
+        }
+        Ok(false)
+    }
 }
 
 #[cfg(test)]
